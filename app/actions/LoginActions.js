3e406714dc0c4c951dd4abc766f6f4696b286c19
Alejandro Caravaca Puchades <alex94puchades@gmail.com>
2015-08-14 03:44:50 +0200

    Refactored services dealing with the avem api

--- app/actions/LoginActions.js
+++ app/actions/LoginActions.js
@@ -2,21 +2,22 @@ import Reflux from 'reflux';
 
 import {AuthService} from '../services';
 
-const actions = Reflux.createActions({
+const actions =  Reflux.createActions({
 	login: { asyncResult: true },
 	logout: { asyncResult: true },
 });
 
 actions.login.listen(function(username, password) {
-	AuthService.login(username, password).then(data => {
-		let accessToken = data.access_token;
-		let refreshToken = data.refresh_token;
+	AuthService.login(username, password).then(response => {
+		let accessToken = response.access_token;
+		let refreshToken = response.refresh_token;
 		this.completed(accessToken, refreshToken);
 	}, this.failed);
 });
 
 actions.logout.listen(function() {
-	AuthService.logout().then(this.completed, this.failed);
+	AuthService.logout()
+	.then(this.completed, this.failed);
 });
 
 export default actions;
---
+++ app/services/ApiService.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+import $ from 'jquery';
+
+import {LoginStore} from '../stores';
+
+class ApiService {
+	request(type, url, opts) {
+		opts = _.defaults({}, opts, {
+			dataType: 'json',
+			contentType: 'application/vnd.api+json',
+		});
+		return $.ajax({
+			type, url,
+			data: opts.data,
+			dataType: opts.dataType,
+			contentType: opts.contentType,
+			beforeSend: jqxhr => {
+				if (!LoginStore.authenticated) return;
+				let accessToken = LoginStore.accessToken;
+				jqxhr.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+			},
+		});
+	}
+
+	errorHandler(callback) {
+		return jqxhr => {
+			return jqxhr.responseJSON
+				? callback(new Error(jsxhr.responseJSON.title))
+				: callback(new Error(jqxhr.responseText));
+		};
+	}
+};
+
+export default new ApiService;
